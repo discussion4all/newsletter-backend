d6d6ab6239115c9caf9d44bd0e87868f656e4fd3
pranavmalvawala <pranavmalvawala@gmail.com>
2020-07-18 10:20:34 +0530

    added support for multiple schedules on the same newsletter or maybe different letters

--- utils/scheduleTexts.js
+++ utils/scheduleTexts.js
@@ -8,12 +8,6 @@ const Newsletter = require("../models/newsletter");
 
 const scheduleTexts = () => {
   setInterval(async () => {
-    console.log("currnt time", new Date());
-    console.log(
-      "currnt time - 5",
-      new Date(new Date().getTime() - 5 * 60 * 1000)
-    );
-
     const schedules = await ScheduleTexts.find({
       completed: false,
       time: {
@@ -24,39 +18,45 @@ const scheduleTexts = () => {
     console.log("schedules", schedules);
 
     if (schedules.length === 0) {
-      console.log("returning...");
+      console.log("No schedules found, returning...");
       return;
     }
 
-    const newsletterId = schedules.map((e) => e.newsletterId);
+    const newsletterIds = schedules.map((e) => e.newsletterId);
+    const subscribers = await getsubs(newsletterIds);
 
-    const subs = await fetchSubs(newsletterId[0]);
-    Promise.all(
-      subs.map((number) => {
-        return client.messages.create({
-          body: schedules[0].text,
-          from: process.env.TWILIO_MESSAGING_SERVICE_SID,
-          to: number,
+    schedules.map(async (schedule) => {
+      const subs = subscribers[schedule.newsletterId];
+      Promise.all(
+        subs.map((number) => {
+          return client.messages.create({
+            body: schedule.text,
+            from: process.env.TWILIO_MESSAGING_SERVICE_SID,
+            to: number,
+          });
+        })
+      )
+        .then(async (messages) => {
+          const updatedNewsletter = await updateTextSent(schedule.newsletterId);
+          const updatedSchedules = await updateScheduleStatus(schedule._id);
+        })
+        .catch((err) => {
+          console.log(err);
         });
-      })
-    )
-      .then(async (messages) => {
-        console.log("messages sent!!", messages);
-        const updatedNewsletter = await updateTextSent(newsletterId[0]);
-        const updatedSchedules = await updateScheduleStatus(schedules[0]._id);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+    });
   }, 60 * 1000);
 };
 
-async function fetchSubs(newsletterId) {
-  const newsletter = await Newsletter.findOne({
-    newsletterId,
+async function getsubs(newsletterIds) {
+  const newsletters = await Newsletter.find({
+    newsletterId: newsletterIds,
+  });
+  let subsObject = {};
+  const subs = newsletters.map((e) => {
+    subsObject[e.newsletterId] = e.subscribers;
   });
 
-  return (newsletter && newsletter.subscribers) || [];
+  return subsObject;
 }
 
 function updateTextSent(newsletterId) {
